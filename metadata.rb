47cdc3592e96fa220295fc4b83f281316c4af99b
Steve Nolen <technolengy@gmail.com>
2015-06-05 17:38:22 -0700

    actually, it turns out we don't need to do this for the package deps. #1

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'technolengy@gmail.com'
 license 'Apache 2.0'
 description 'Installs/Configures cens-rstudio'
 long_description 'Installs/Configures cens-rstudio'
-version '0.0.7'
+version '0.0.8'
 
 %w(ubuntu).each do |os|
   supports os
