65da777b0b72f1f15948c66a00abc91c6a4ea9fc
Steve Nolen <technolengy@gmail.com>
2015-07-18 09:28:42 -0700

    reverting to http for now, our libcurl isn't new enough?

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'technolengy@gmail.com'
 license 'Apache 2.0'
 description 'Installs/Configures cens-rstudio'
 long_description 'Installs/Configures cens-rstudio'
-version '0.0.26'
+version '0.0.27'
 
 %w(ubuntu).each do |os|
   supports os
